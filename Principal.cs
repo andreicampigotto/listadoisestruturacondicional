a9d4ca00b159915ba8e7e93fdcc2f6b15740b740
Alan eduardo alves <alaneduardoalves2018@gmail.com>
2018-04-24 17:11:25 -0300

    Lista 2 até numero 7
    terminar até 25/04

---
+++ Principal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosListadeEstruturaCondicional
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            //new Exercicio01();
+            //new Exercicio02();
+            //new Exercicio03();
+            //new Exercicio04();
+            new Exercicio06();
+
+
+        }
+    }
+}
